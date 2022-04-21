0afddfa7c667a136017d520e637fc60c87682ad4
Victor Wåhlstrand Skärström <victor.wahlstrand@gmail.com>
2022-04-21 15:19:50 +0200

    Added position endpoints

--- api.py
+++ api.py
@@ -7,19 +7,21 @@ import pandas as pd
 import requests
 from pydantic import BaseModel
 import numpy as np
-from .settings import DATA_URL, KNN_INDEX_PATH
+import json
+
+# Load the configuration
+with open("./configs/urls.json", 'r+') as f:
+    config = json.load(f)
 
 # Get the review data
-response = requests.get(DATA_URL)
+response = requests.get(config["DATA_URL"])
 records = response.json()
 df = pd.json_normalize(records)
 df = df.rename({'gender of author': 'gender_of_author', 'gender of critic': 'gender_of_critic', 'same year': 'same_year'}, axis=1)
 
 # Get the nearest neighbours index
-index = np.memmap(KNN_INDEX_PATH, dtype='float32', mode='r+', shape=(len(df), len(df)))
-
+index = np.memmap(config["KNN_INDEX_PATH"], dtype='int32', mode='r+', shape=(len(df), len(df)))
 class Review(BaseModel):
-    id: int
     title: Union[str, None]
     author: Union[str, None]
     forum: Union[str, None]
@@ -36,6 +38,9 @@ class Review(BaseModel):
     class Config:
         orm_mode = True
 
+class Position(BaseModel):
+    x: Union[float, None]
+    y: Union[float, None]
 
 middleware = [Middleware(CORSMiddleware, 
                         allow_origins=['*'], 
@@ -56,6 +61,22 @@ async def get_review(review_id: int):
 
 @app.get("/api/reviews/nearest/", response_model=List[Review])
 async def get_nearest_neighbors(review_id: int, nn: int = 10):
-    review_idxs = index[review_id][:nn]
+    review_idxs = index[review_id, :nn]
 
     return list(df.iloc[review_idxs].to_dict('records'))
+
+@app.get("/api/reviews/{review_id}/position/", response_model=Position)
+async def get_position_of_review(review_id: int):
+
+    return {
+        "x": df.iloc[review_id].x,
+        "y": df.iloc[review_id].y
+    }
+
+@app.get("/api/positions/", response_model=List[Position])
+async def get_positions_of_reviews(title: str):
+
+    if title:
+        results = df[df.title == title][['x', 'y']]
+
+    return list(results.to_dict('records'))
